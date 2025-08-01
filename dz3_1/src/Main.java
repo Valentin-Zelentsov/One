39c5b2d8f0802cd4feb6b4b4393fcaa2d8f62477
Valentin Zelentsov <valentin.zelentsov@nw.rt.ru>
2025-08-01 21:37:31 +0300

    DZ3_1

---
+++ dz3_1/src/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Введите имя:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Привет, "+scanner.nextLine());
+
+    }
+}
