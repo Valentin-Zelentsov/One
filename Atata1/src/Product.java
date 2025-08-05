22758216d657d1121153f668e8640ad6bd138761
Valentin Zelentsov <Valentin.Zelentsov@nw.rt.ru>
2025-08-05 23:07:33 +0300

    att1

---
+++ Atata1/src/Product.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private int cost;
+
+    public Product(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, cost);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
